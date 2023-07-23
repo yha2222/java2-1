d002cf56528740209fb53f5048f0c39ffb0b3b48
yha2222 <yhaha5751@gmail.com>
2023-07-23 23:44:46 +0900

    로또 끝 - 프로그램 완전히 꺼지게?

--- JavaCollectionTest/src/kr/or/ddit/basic/Report0720.java
+++ JavaCollectionTest/src/kr/or/ddit/basic/Report0720.java
@@ -1,36 +1,82 @@
 package kr.or.ddit.basic;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Report0720 {
-
 	public static void main(String[] args) {
-		
-		
-		//프린트
-		//변수 설정 //받은 금액 , 거스름돈, 횟수
-		//if 1이면 구입 시작 화면 
-		// List<List<Integer>> lottoList = getLotto(count);
-		//로또 번호 보여주기
-		//향상된 for문으로 로또 번호 출력
-		//받은 금액, 거스름돈 출력
-		//2면 감사합니다하고 종료 System.exit(0);
-		//아니면 잘못된 숫자 입력
-		
-		
-		Set<Integer> lotto = new HashSet<Integer>();
-		
-		while(lotto.size() < 6) {
-			int num = (int)(Math.random() * 45 + 1);
-			lotto.add(num);
-		}
-		
-		System.out.println("로또번호1: " + lotto);
-		System.out.println("로또번호2: " + lotto);
-		
-		
-
+		new Report0720().lottoStart();
 	}
+
+	private Scanner scan;
+	int money;
+	int cnt;
+	int change;
+	
+	public void menu(){
+		System.out.println();
+		System.out.println("=================================");
+		System.out.println("        Lotto 프로그램");
+		System.out.println("---------------------------------");
+		System.out.println("        1. Lotto 구입");
+		System.out.println("        2. 프로그램 종료");
+		System.out.println("=================================");
+		System.out.println("메뉴선택 : ");
+	}
+	
+	public void lottoStart() {
+		scan = new Scanner(System.in);
+		
+		while(true) {
+			menu();
+			
+			int menuNum = scan.nextInt();
+			
+			switch(menuNum) {
+			case 1: purch();
+				break;
+			case 2 : 
+				System.out.println("감사합니다");
+				break;
+			default : 
+				System.out.println("잘못된 입력!");
+			}
+		}
+	}
+	
+	private void purch() {
+		scan = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("Lotto 구입 시작");
+		System.out.println("(1000원에 로또번호 하나입니다.)");
+		System.out.println("금액 입력 : ");
+		
+		money = scan.nextInt();
+		change = money % 1000;
+		
+		System.out.println("행운의 로또번호는 아래와 같습니다.");
+		printLotto();
+		System.out.println("받은 금액은 "+money+"원이고 거스름돈은 "+change+"원입니다.");
+		
+	}
+	
+	private void printLotto() {
+		cnt = money / 1000;
+		
+		for(int i = 0; i < cnt; i++) {
+			Set<Integer> lotto = new HashSet<Integer>();
+			
+			while(lotto.size() < 6) {
+				int num = (int)(Math.random() * 45 + 1);
+				lotto.add(num);
+			}
+			
+		 System.out.println("로또번호" + (i+1) +": " + lotto);
+		}
+	}
+
+	
 
 }
